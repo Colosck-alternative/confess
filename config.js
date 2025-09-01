9e898cfe622cfdaeed483336e652750aa61a1035
MinhLong2410-02 <minhlong2002@gmail.com>
2025-09-01 13:26:19 +0700

    first init

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Tôi muốn làm người yêu Quỳnh",
+    introTitle: 'Nguyễn Xuân Quỳnh',
+    introDesc: `Lười qua nên lấy cái web tỏ tình cho nhanh`,
+    btnIntro: 'Hẹ hẹ',
+    title: 'ý là Long cũng muốn có danh phận rồi ấy =))))',
+    desc: 'Nên là bé Quỳnh trả lời lại anh Long trong 1/9 nhaaaaaa',
+    btnYes: 'Sến quá z, bé Quỳnh thích lắm',
+    btnNo: 'Không nha :3',
+    question: 'Kiểu gì cũng muốn ấn nút từ chối, cho hỏi tại sao?',
+    btnReply: 'Gửi câu trả lời cho Long <3',
+    reply: 'Tại không cho đjt á :)))',
+    mess: 'Tôi biết mà 🥰. Hóa ra tính Quỳnh là z 😘😘',
+    messDesc: 'Thôi Quỳnh đợi tuần sau nhaaaa.',
+    btnAccept: 'Okiiiii lun <3 (sến vc ewwww)',
+    messLink: 'https://www.facebook.com/messages/e2ee/t/1113481310181441' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
